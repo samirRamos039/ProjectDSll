db797009330e9de5200fc4533994820abae11a03
rzagza039@gmail.com <rzagza039@gmail.com>
2022-11-27 16:32:46 -0500

    probando workflow

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 
 const express = require('express')
 const morgan = require("morgan");
+const cors = require('cors')
 
 const tasksRouts = require('./routes/tasks');
  
  const app = express();
- app.use(morgan('dev'))
- app.use(express.json())
+
+ app.use(cors());
+ app.use(morgan('dev'));
+ app.use(express.json());
  app.use(tasksRouts)
- app.listen(3000)
- console.log('server on port 3000')
+ app.listen(4000)
+ console.log('server on port 4000')
  
